df556a8f7a004ac5cbdb73a4e65a7b3dc14156f1
Bartosz Stasik <bartolus99@gmail.com>
2018-12-13 12:47:24 +0000

    Change minor formatting.

--- unix/src/titles/title.sh
+++ unix/src/titles/title.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-progress_bar()
-{
+progress_bar() {
 	printf "Loading... | "
 	for x in {1..53}; do
 		printf "#"
 		sleep .01
 	done ; echo
 }
-title_screen()
-{
+title_screen() {
 	printf "\e[H\e[J\n"
 	cat "$1"
 	printf "\n"
