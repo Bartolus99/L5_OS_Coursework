a6edc744a7b280644c7764325c3a246063a10643
Bartosz Stasik <bartolus99@gmail.com>
2018-12-15 18:26:54 +0000

    Add annotation and credits

--- unix/src/menus/ip/ip-block.sh
+++ unix/src/menus/ip/ip-block.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
 
-runnable=0
+# Author Bartosz Stasik - U1730148
+# Co-Author Joshua Button - U1628860 - www.JoshuaButton.co.uk
+
+
+############
+#References#
+############
+
+# ANSI Escape Sequences - https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+# cURL - https://curl.haxx.se/docs/manpage.html
+# Incrementing Variables - https://askubuntu.com/questions/385528/how-to-increment-a-variable-in-bash
+# IP Route - http://linux-ip.net/html/tools-ip-route.html
+
+
+##################
+#Get Title Screen#
+##################
 
 title_screen() {
+	# Title files are loaded with the title.sh script.
+	# The title.sh script takes the ASCII title path as a parameter.
 	eval "$TITLE_PATH/title.sh $TITLE_PATH/ip-block"
 }
+
+####################
+#Check Dependencies#
+####################
+
 checkCurlInstalled() {
+	# Using ldconfig to print the list the libraries stored the in the cache.
+	# The standard output is piped to grep, which curl for entries with "curl" in them.
+	# If the standard output is empty, cURL is clearly not installed. 
+	# The runnable variable is used to keep track of the missing dependencies.
 	printf "\n"
 	printf "Checking for dependencies...\n"
 	if [ "$(ldconfig -p | grep curl)" == "" ] ; then
@@ -15,7 +42,15 @@ checkCurlInstalled() {
 		runnable=$((runable+1))
 	fi
 }
+
+##################
+#Update Blacklist#
+##################
+
 updateBlacklist() {
+	# The blacklist is simply downloaded using cURL. 
+	# The user's IP address is gathered from ipinfo.io and is used in the log.
+	# The standard output of cURL command, used to download the blacklist is appended to the log file.
 	printf "Updating blacklist...\n"
 	printf "\n"
 	{
@@ -27,7 +62,18 @@ updateBlacklist() {
 	curl -O "http://myip.ms/files/blacklist/htaccess/latest_blacklist.txt" >> "$LOG_PATH/blacklist.log" 2>&1 
 	printf "\nDone!\n"
 }
+
+#######################
+#Block Blacklisted IPs#
+#######################
+
 blockFromBlacklist() {
+	# A while loop is used to read the latest_blacklist.txt file.
+	# The lines are split, using the " " delimiter and, therefore, each word is put into
+	# the "f1", "f2" and "f3" variables, of which "f3" is the IPv4/IPv6 address.
+	# Based on the output of "ip route", the file shown to either be "already blocked" or "blocked
+	# successfully".
+	# The phrases "already blocked" and "blocked successfully" are colors used ANSI Escape Sequences.
 	file="latest_blacklist.txt"
 	while IFS=" " read -r f1 f2 f3 ; do
 		if [ "$f1" == "deny" ]; then
@@ -41,16 +87,31 @@ blockFromBlacklist() {
 	done < "$file"
 }
 
+#################
+#Build Interface#
+#################
+
+# The script must be executed from the menu.sh script, its relative path to this script being 
+# "../../../menu.sh". 
+# The "IP_PATH" constant is used to point to the path relative to the "src" folder.
+# The other constants point to the paths relative to this script.
 IP_PATH="./menus/ip"
 TITLE_PATH="./titles"
 LOG_PATH="./logs"
 
+# Runnable is used as tracker for missing dependencies.
+runnable=0
+
+# Loading the title screen by calling the title_screen function
 title_screen
 
+# Changing the directory to that of this script, in order to be able to use relative rather than
+# absolute paths.
 cd $IP_PATH
 
+# Checking for dependencies and using the "runnable" variable to make sure that the script doesn't run
+# unless they are installed.
 checkCurlInstalled
-
 if [ "$runnable" -gt "0" ] ; then
 	updateBlacklist
 	blockFromBlacklist
--- unix/src/menus/pass/pass-check.sh
+++ unix/src/menus/pass/pass-check.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
+# Author Bartosz Stasik - U1730148
+# Co-Author Joshua Button - U1628860 - www.JoshuaButton.co.uk
+
+
+############
+#References#
+############
+
+# ANSI Espace Sequences - https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+# Regular Expressions - http://tldp.org/LDP/abs/html/x17129.html
+# Regular Expressions - http://tldp.org/LDP/abs/html/x17129.html
+# Valid Password Characters - https://www.ibm.com/support/knowledgecenter/en/SSFPJS_8.5.7/com.ibm.wbpm.imuc.doc/topics/rsec_characters.html
+
+
+##################
+#Get Title Screen#
+##################
+
 title_screen() {
+	# Title files are loaded with the title.sh script.
+	# The title.sh script takes the ASCII title path as a parameter.
 	eval "$TITLE_PATH/title.sh $TITLE_PATH/pass-check"
 }
 
+#################
+#Build Interface#
+#################
+
+# The script must be executed from the menu.sh script; its relative path to this script being
+# "../../../menu.sh".
+# The "TITLE_PATH" constant is used to point to the path relative to the "src" folder.
 TITLE_PATH="./titles"
 
+# Loading the title screen by calling the title_screen function.
 title_screen
 
+# Displaying the disclaimer and prompt. 
+# No specific validation required, as the code uses REGEX to check complexity.
 printf "\n\e[37mIMPORTANT: \e[0mYour password will \e[1;31mNOT\e[0m be displayed or saved anywhere!\n"
 printf "\nEnter a password to check: "
 read -s password
 printf "\n"
 
+# The "complexity" variable is used to keep track of the password "score".
 complexity="0"
 if [ "${#password}" -le 7 ] ; then
 	complexity="-1"
@@ -35,7 +66,8 @@ else
 fi
 
 
-
+# Based on the complexity "score", the user is presented  with different answers.
+# Results are coloured using ANSI Escape Sequences (following \e).
 case "$complexity" in
 	"-1")
 		printf "\nYour password is \e[1;31mweak!\e[0m\n"
